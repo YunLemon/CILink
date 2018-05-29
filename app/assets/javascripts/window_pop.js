6ab1e249513d6c809a5b3ba100c7eb2529bffa8c
zc <340355960@qq.com>
2018-05-29 14:51:56 +0800

    14:51

--- app/assets/javascripts/window_pop.js
+++ app/assets/javascripts/window_pop.js
@@ -1,3 +1,13 @@
-function showMessage(){
-    alert("test");
-}
+console.log("window_pop.js loaded");
+
+var ready;
+ready = function() {
+
+  $("#test").on("click", function(event) {
+    alert("link clicked");
+  });
+
+};
+
+$(document).ready(ready);
+$(document).on("page:load", ready);
