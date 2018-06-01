e7e05b6aa4cc84ea46dface79921db9bc98580b4
zc <340355960@qq.com>
2018-06-01 15:02:26 +0800

    14:41

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,7 +3,8 @@ class LogsController < ApplicationController
         @job = JavaRepoJobDatum.find(params[:job_id])
         repo_name = @job.repo_name
         job_number = @job.job_number
+        @slice_segment = @job.slice_segment
         #render json: @job
-        send_file "/home/fdse/user/zc/bodyLog2/build_logs/#{repo_name.sub(/\//, '@')}/#{job_number.sub(/\./, '@')}.log", type: 'text/plain; charset=utf-8', disposition: 'inline'
+        @log_file = IO.read "/home/fdse/user/zc/bodyLog2/build_logs/#{repo_name.sub(/\//, '@')}/#{job_number.sub(/\./, '@')}.log", type: 'text/plain; charset=utf-8', disposition: 'inline'
     end
 end
