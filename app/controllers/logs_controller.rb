d1ff0234608705d8d7c8b43065ab4052130e1954
zc <340355960@qq.com>
2018-06-01 14:41:58 +0800

    14:41

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,8 +1,9 @@
 class LogsController < ApplicationController
     def show
         @job = JavaRepoJobDatum.find(params[:job_id])
-        @matches = job.compiler_error_matches
-        render json: match
-        #send_file "/home/fdse/user/zc/bodyLog2/build_logs/#{params[:repo].sub(/\//, '@')}/#{params[:job_number].sub(/\./, '@')}.log", type: 'text/plain; charset=utf-8', disposition: 'inline'
+        repo_name = @job.repo_name
+        job_number = @job.job_number
+        #render json: @job
+        send_file "/home/fdse/user/zc/bodyLog2/build_logs/#{repo_name.sub(/\//, '@')}/#{job_number.sub(/\./, '@')}.log", type: 'text/plain; charset=utf-8', disposition: 'inline'
     end
 end
