1fbb4622ae877f9a537f4e80884999a70bb4e7e6
zc <340355960@qq.com>
2018-06-01 21:41:54 +0800

    21:41

--- app/assets/javascripts/window_pop.js
+++ app/assets/javascripts/window_pop.js
@@ -7,14 +7,3 @@ $(function(){
     $(this).popover("hide");
   }); 
 });
-$(document).ready(function() {
-  //var stream = new AnsiStream()
-
-  //onStreamText: (text) ->
-  //  $('.code').append(stream.process(text))
-});
-
-
-$(document).ready(function() {
-  $("#markItUp").markItUp(mySettings);
-});
